1ba4500ed3a59d38f6f1c263adef35e3d9af56f6
marte <martet@ttu.ee>
2021-05-03 00:03:46 +0300

    Add customOption which changes color after clicking to see which sectors are chosen

---
+++ src/components/CustomOption.vue
@@ -0,0 +1,23 @@
+<template>
+  <option :style="isClicked ? { 'color': '#45ed32' } : null" @click="toggleIsClicked">
+    {{sectorName}}
+  </option>
+</template>
+
+<script>
+export default {
+  name: 'CustomOption',
+  props: ['sectorName', 'sectorObj'],
+  data () {
+    return {
+      isClicked: false
+    }
+  },
+  methods: {
+    toggleIsClicked () {
+      this.isClicked = !this.isClicked
+      this.$emit('event', this.sectorObj)
+    }
+  }
+}
+</script>
